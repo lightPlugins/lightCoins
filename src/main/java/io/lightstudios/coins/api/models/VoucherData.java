b7957ab66bc879cdacc66fedba248bf93b071711
lightplugins <lightningdesign@outloo.de>
2025-01-05 20:34:51 +0100

    prepare voucher system

---
+++ src/main/java/io/lightstudios/coins/api/models/VoucherData.java
@@ -0,0 +1,15 @@
+package io.lightstudios.coins.api.models;
+
+import org.bukkit.inventory.ItemStack;
+
+public class VoucherData {
+
+    private boolean enabled;
+    private ItemStack voucherItem;
+    private String currencyName;
+    private double minValue;
+    private double maxValue;
+    private int commandCooldown;
+
+
+}
