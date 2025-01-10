e4f75ff9df26bc5ed63f562eb09136974f498696
lightplugins <lightningdesign@outloo.de>
2025-01-10 07:15:45 +0100

    removed spigot-api and replaced it with paper-api

--- src/main/java/io/lightstudios/coins/api/models/VoucherData.java
+++ src/main/java/io/lightstudios/coins/api/models/VoucherData.java
@@ -1,15 +1,29 @@
 package io.lightstudios.coins.api.models;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
 
 public class VoucherData {
 
+    private File file;
     private boolean enabled;
     private ItemStack voucherItem;
     private String currencyName;
     private double minValue;
     private double maxValue;
     private int commandCooldown;
+    private boolean vaultEconomy;
 
+
+    public VoucherData(File file) {
+        this.file = file;
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        this.enabled = config.getBoolean("enabled");
+
+    }
 
 }
