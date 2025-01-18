385cb3107ce71a1420c82ee7997c59e488cddf53
lightplugins <lightningdesign@outloo.de>
2025-01-18 14:51:55 +0100

    migrated maven to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    java
+    id("io.freefair.lombok") version "8.11"
+}
+
+group = "io.lightstudios.coins"
+version = "0.0.7"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        name = "codemc"
+        url = uri("https://repo.codemc.org/repository/maven-public")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+        exclude(group = "org.bukkit", module = "bukkit")
+    }
+    compileOnly("com.github.lightPlugins:lightCore:0.3.8")
+    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.9")
+}
+
+tasks {
+    processResources {
+        from(sourceSets.main.get().resources.srcDirs()) {
+            filesMatching("plugin.yml") {
+                expand(
+                    "name" to rootProject.name,
+                    "version" to rootProject.version
+                )
+
+            }
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
